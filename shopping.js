67ba7845077de2cfb771fb92234e1ebabf2d9028
Yue Wu <wuyue681@gmail.com>
2012-11-23 12:37:21 +0000

    测试链接和事件。

--- shopping.js
+++ shopping.js
@@ -2,4 +2,8 @@ $(document).bind('pageinit', function () {
     $('a#item-submit').bind('tap', function () {
 	$('form#new-item-form').submit();
     });
+
+    $('a#item-submit').bind('click', function () {
+	$('form#new-item-form').submit();
+    });
 });
